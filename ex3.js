8c1624cc0423313575c11c9db1ba8a37b319fac6
Bharti685-create <70859447+Bharti685-create@users.noreply.github.com>
2021-03-03 17:02:19 -0800

    Delete ex3.js

--- ex3.js
+++
@@ -1,9 +0,0 @@
-/* Shinde_hw6
- Example 3 JavaScript code
-*/
-console.log("Bharti's Output from Homework 6 Example 3");
-
-const paraElements = document.querySelectorAll("p");
-for (x of paraElements){
-    x.style.backgroundColor = "yellow" ; x.style.fontWeight = "bold"
-}
