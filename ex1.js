a1cc32afdb363a304f82f104ff679f222aec4be4
Bharti685-create <70859447+Bharti685-create@users.noreply.github.com>
2021-03-03 16:03:50 -0800

    Delete ex1.js

--- ex1.js
+++
@@ -1,8 +0,0 @@
-/* Shinde_hw6
- Example 1 JavaScript code
-*/
-console.log("Bharti's Output from Homework 6 Example 1");
-
-for (const bodyNodes of document.body.childNodes
-){console.log(bodyNodes)
-} 
